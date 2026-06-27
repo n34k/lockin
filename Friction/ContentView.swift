13a746d9d1d3a70e5c5e08ed635b2c7bc54ddfce
Nick Davis <ndavis55862@gmail.com>
2026-06-26 19:36:46 -0700

    Initial Commit

---
+++ Friction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friction
+//
+//  Created by Nick Davis on 6/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Friction/FrictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrictionApp.swift
+//  Friction
+//
+//  Created by Nick Davis on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct FrictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
